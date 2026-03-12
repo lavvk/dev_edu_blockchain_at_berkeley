9fdab5db8872f721d3319d7ee90f222f4d32fbb8
lavanya <161574676+lavvk@users.noreply.github.com>
2026-03-12 16:55:23 -0700

    Delete global.d.ts

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-// Ensure JSX intrinsic elements are defined so TS can type-check JSX.
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      [elemName: string]: any;
-    }
-  }
-}
-
