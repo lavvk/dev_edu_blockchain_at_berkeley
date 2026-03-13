c0474b957793e1f59eab62fc7cfa53f41f800d5f
lavanya <161574676+lavvk@users.noreply.github.com>
2026-03-12 17:03:58 -0700

    Update AboutPage text content

--- dev-edu1/app/about/page.tsx
+++ dev-edu1/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function AboutPage() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-pink-100">
       <h2 className="text-4xl font-bold text-black">
-        This is lavanya I built this
+        hello i am lavanya i built this i love b@b 
       </h2>
     </main>
   );
